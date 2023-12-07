609459b22a4b31abbe1637c2715a46f5865fdaee
Mrbilal <bilal_itchir@Outlook.fr>
2023-12-07 20:16:02 +0100

    🐛 corriger(index.php) : corriger la condition pour vérifier si les noms des joueurs sont définis avant d'inclure le fichier Game.php ✨ fonctionnalité(index.php) : ajouter la gestion du score des joueurs en utilisant les variables de session score1 et score2 ✨ fonctionnalité(index.php) : ajouter la gestion de l'état du choix des joueurs en utilisant la variable de session state

--- index.php
+++ index.php
@@ -4,34 +4,40 @@ require 'classes/Choices.php';
 
 use Shifumi\Choices;
 use Shifumi\Players;
-session_start();
 
-$choices = new Choices(
-    '',
-    '',
-    rand(0, 1)
-);
-if (isset($_POST['name1']) || isset($_POST['name2']) || isset($_SESSION['name1'])) {
-    include 'Pages/Game.php';
+session_start();
+$choices = new Choices('', '');
+
+if (isset($_POST['name1']) && isset($_POST['name2']) || isset($_SESSION['name1'])) {
+    $score1 = $_SESSION['score1'] ?? 0;
+    $score2 = $_SESSION['score2'] ?? 0;
+    //Premier Tour
     if (!isset($_SESSION['name1'])) {
-        $score = 0;
+        $players = new Players(
+            $_POST['name1'],
+            $_POST['name2'],
+            $score1,
+            $score2
+        );
+        $choices->setState(rand(0,1));
+        $playerName = [$players->getName1(), $players->getName2()];
+        $name = $playerName[$choices->getState()];
+    }
+    //Deuxième Tour
+    if (isset($_SESSION['name1'])) {
         $players = new Players(
             $_SESSION['name1'],
             $_SESSION['name2'],
-            $score,
+            $score1,
+            $score2
         );
+        $choices->setState($_SESSION['state']);
+        $choices->getState() == 0 ? $choices->setState(1) : $choices->setState(0);
         $playerName = [$players->getName1(), $players->getName2()];
-        if ($players->getName1() && $players->getName2() ) {
-            $name = $playerName[$choices->getState()] ;
-        }
+        $name = $playerName[$choices->getState()];
     }
-    if (isset($_SESSION['name1'])) {
-        $name = $_SESSION['name1'];
-        if ($_POST['choice']){
-
-        }
-    }
-
+    include 'Pages/Game.php';
+    var_dump($_SESSION);
 
 } else {
     include './Pages/Accueil.php';
